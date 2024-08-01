90ebf0bc87b911ff4daaa398b16d9b56f0896846
Satish Rathod <satishrathod.dev@gmail.com>
2024-08-01 06:22:12 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
+import "regenerator-runtime/runtime";
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
+import axios from "axios";
 
-import React from "react";
-import './../styles/App.css';
+const fetchURL = "https://reqres.in/api/users";
 
 const App = () => {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    // const getData = axios.get(fetchURL).then((response) => {
+    //   setPost(response.data);
+    //   console.log(response.data);
+    // });
+    const getData = async () => {
+      try {
+        const response = await axios.get(fetchURL);
+        setPost(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getData();
+  }, []);
+  // const getData = async () => {
+  //   const response = await axios.get("https://reqres.in/api/users");
+  //   console.log(response);
+  // };
+
+  // if (!post)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <pre>{post ? JSON.stringify(post, null, 2) : "Loading..."}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
