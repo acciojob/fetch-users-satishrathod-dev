9cbc18f0790e341cba511edfec72c2aaf89b5bf8
Satish Rathod <satishrathod.dev@gmail.com>
2024-10-23 07:39:26 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -56,7 +56,7 @@ const App = () => {
             ))
           ) : (
             <tr>
-              <td colSpan="4">No data found to display</td>
+              <td colSpan="4">No data found to display.</td>
             </tr>
           )}
         </tbody>
